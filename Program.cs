3fc9dbcc32caa7989d6f84d21fb4bf0f39c4d82d
rsmo <robert.smolcic@holosys.hr>
2019-08-26 12:44:45 +0200

    Finished first video

--- Program.cs
+++ Program.cs
@@ -2,11 +2,29 @@
 
 namespace Classes
 {
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var person = Person.Parse("John");
+            person.Introduce("Mosh");
         }
     }
 }
