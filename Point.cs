751ba2e75b9950f7b96b0061cf195ae0e7f8cc95
rsmo <robert.smolcic@holosys.hr>
2019-08-27 14:07:16 +0200

    Finished methods video

---
+++ Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Point
+    {
+        public int X;
+        public int Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public void Move(int x, int y)
+        {
+            this.X = x; 
+            this.Y = y;
+        }
+
+        public void Move(Point newLocation)
+        {
+            if (newLocation == null)
+                throw new ArgumentNullException("newLocation");
+            Move(newLocation.X, newLocation.Y);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -20,20 +20,78 @@ namespace Classes
         }
     }
 
+    public class Calculator
+    {
+        public int Add(params int[] numbers)
+        {
+            var sum = 0;
+            foreach (var number in numbers)
+            {
+                sum += number;
+            }
+
+            return sum;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             var person = Person.Parse("John");
-            person.Introduce("Mosh");
+            //person.Introduce("Mosh");
 
             var customer = new Customer(1, "John");
 
             var order = new Order();
             customer.Orders.Add(order);
 
-            Console.WriteLine(customer.Id);
-            Console.WriteLine(customer.Name);
+            //Console.WriteLine(customer.Id);
+            //Console.WriteLine(customer.Name);
+            try
+            {
+                var num = int.Parse("abc");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Conversion failed.");
+            }
+            
+
+
+            int number;
+            var result = int.TryParse("abc", out number);
+            if (result)
+                Console.WriteLine(number);
+            else
+                Console.WriteLine("Conversion failed.");
+
+
+        }
+
+        static void UseParams()
+        {
+            var calculator = new Calculator();
+            Console.WriteLine(calculator.Add(1, 2));
+            Console.WriteLine(calculator.Add(1, 2, 3));
+            Console.WriteLine(calculator.Add(1, 2, 3, 4, 5));
+            Console.WriteLine(calculator.Add(new int[] { 1, 2, 3, 6, 7, 9 }));
+        }
+
+        static void UsePoints()
+        {
+            try
+            {
+                var point = new Point(10, 20);
+                point.Move(null);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+
+                point.Move(100, 200);
+                Console.WriteLine("Point is at ({0}, {1})", point.X, point.Y);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An unexpected error occured.");
+            }
         }
     }
 }
