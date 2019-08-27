10f9aac133da6ad6ec3408f43bcfee9b33167a7c
rsmo <robert.smolcic@holosys.hr>
2019-08-27 12:52:03 +0200

    Finished constructors video

---
+++ Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders; // This has to be initialized, because the default value will be null, and we would get a null reference.
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id)
+            : this()
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+            : this(id) 
+        {
+            this.Name = name;
+        }
+
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,12 +19,21 @@ namespace Classes
             return person;
         }
     }
+
     class Program
     {
         static void Main(string[] args)
         {
             var person = Person.Parse("John");
             person.Introduce("Mosh");
+
+            var customer = new Customer(1, "John");
+
+            var order = new Order();
+            customer.Orders.Add(order);
+
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
         }
     }
 }
